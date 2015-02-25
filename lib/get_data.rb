c3aa5daa1512eddd578013eb19a476dacd5326aa
Scott <scott@dottedt.com>
2015-02-24 21:12:49 -0600

    initial setup for getting data

---
+++ lib/get_data.rb
@@ -0,0 +1,22 @@
+class GetData
+  def data_type(input)
+    menu_type = input
+    case
+    when menu_type.kind_of?(Array)
+      input.each_with_index do |menu_item, i|
+        self.price_goal = menu_item[0] if i == 0
+
+      end
+
+    when menu_type.kind_of?(Hash)
+      input.each_with_index do |menu_item, i|
+        self.price_goal = menu_item[0] if i == 0
+      end
+
+    else
+      File.open(input).each_with_index do |menu_item, i|
+        self.price_goal = menu_item[0] if i == 0
+      end
+    end
+  end
+end
